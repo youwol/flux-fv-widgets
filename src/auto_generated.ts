784906433253afcd97df92e805273e90c86a2002
greinisch <greinisch@youwol.com>
2021-07-13 09:08:42 +0200

    :zap: include auto-generated file

---
+++ src/auto_generated.ts
@@ -0,0 +1,6 @@
+export let AUTO_GENERATED = {
+    name: "@youwol/flux-fv-widgets",
+    namespace: "youwol",
+    version: "0.0.4",
+    description: "Collection of basic widgets (essentially from @youwol/fv-widgets)"
+}
