f9edd270fba872a652e5986ef9be6e0c6c1324a3
greinisch <greinisch@youwol.com>
2021-10-06 14:42:40 +0200

    :bookmark: 0.0.7

--- src/auto_generated.ts
+++ src/auto_generated.ts
@@ -1,6 +1,6 @@
 export let AUTO_GENERATED = {
     name: "@youwol/flux-fv-widgets",
     namespace: "youwol",
-    version: "0.0.6",
+    version: "0.0.7",
     description: "Collection of basic widgets (essentially from @youwol/fv-widgets)"
 }
