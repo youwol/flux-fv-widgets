9747e2c3079934f33756ce14901ee1f0e01305f3
greinisch <greinisch@youwol.com>
2021-10-15 15:49:30 +0200

    ➕ export autoform-view

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ export * from './lib/slider.module';
 export * from './lib/button.module';
 export * from './lib/select.module';
 export * from './lib/auto-form.plugin';
+export * from './lib/auto-form/auto-form.view';
+
